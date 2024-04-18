8313a6668cb827febd4850aa8e52ffeda86ba559
Juan Bautista <jbautistapulido@gmail.com>
2024-04-18 16:12:34 -0500

    Operators JavaScript

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -1,2 +1,26 @@
 // Arreglos en JavaScript
 // Diferencias entre arreglos y listas
+
+const arrayLleno = [1, 2, 4, "Hola", "mundo"] // Asi se define un erreglo
+const arrVacio = []
+
+// Un arreglo contiene datos de distintos tipos
+
+console.log(arrayLleno, arrVacio)
+
+function Array(array) {
+    for (let index = 0; index < array.length; index++) {
+          console.log(array[index])
+        
+    }
+}
+
+console.log(Array(arrayLleno));
+
+// La funcion push() agrega elementos al arreglo
+
+const variable = "La variable"
+arrVacio.push(5)
+arrVacio.push(variable)
+
+console.log(arrVacio)
---
+++ JavaScript/Operadores.js
@@ -0,0 +1,25 @@
+function Suma(n1,n2){
+    return n1 + n2;
+}
+
+function Resta(n1,n2){
+    return n1 - n2;
+}
+
+
+function Multiplicacion(n1,n2){
+    return n1 * n2;
+}
+
+function Division(n1,n2){
+    return n1 / n2;
+}
+
+
+console.log(Suma(2 ,2),Resta(5,3),Multiplicacion(5,4),Division(5,2))
+
+// ------------------ OPERADORES DE COMPARACIÓN ----------------------
+
+// Los operadores de comparación permite 
+
+
