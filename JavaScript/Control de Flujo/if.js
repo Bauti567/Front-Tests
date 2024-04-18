593c3ac3029176f72948d5b41848ca4be4cdbcb9
Juan Bautista <jbautistapulido@gmail.com>
2024-04-18 17:52:06 -0500

    New files from operators

---
+++ JavaScript/Control de Flujo/if.js
@@ -0,0 +1,16 @@
+// El condicional if me permite evaluar en caso de que cambie
+// Alguna de las secciones dentro del codigo
+
+// Escribir un programa que adivine si es mayor o no de edad
+function Mayor(edad) {
+    if (edad >= 18){
+        console.log('Damn youre 18', edad)
+    } else{
+        console.log('You cand your age is', edad)
+    }
+}
+
+console.log(Mayor(18))
+
+// Escribir un programa que ingrese una contraseña y retorne si la contraseña es correcta
+
