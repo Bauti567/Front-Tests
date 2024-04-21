948a4b0c4fa987b9e1a65c99916a4865a29cd708
Juan Bautista <jbautistapulido@gmail.com>
2024-04-21 14:40:21 -0500

    New exercices control Pipes

--- JavaScript/Control de Flujo/if.js
+++ JavaScript/Control de Flujo/if.js
@@ -2,6 +2,9 @@
 // Alguna de las secciones dentro del codigo
 
 // Escribir un programa que adivine si es mayor o no de edad
+
+
+// Programa que calcula si es mayor de edad
 function Mayor(edad) {
     if (edad >= 18){
         console.log('Damn youre 18', edad)
@@ -10,7 +13,34 @@ function Mayor(edad) {
     }
 }
 
-console.log(Mayor(18))
+// Programa que revisa que la contraseña sea conrrecta
+let contraseña = 12345;
+function Contraseña(contraseña) {
+    if (contraseña === 12345 ) {
+        console.log('La contraseña es correcta')
+    } else{
+        console.log('La contraseña es incorrecta')
+    }
+}
 
+// Funcion que divide dos numeros
+function Division(n1,n2) {
+    res = n1/n2
+    if (n1 === 0 || n2 === 0) {
+        console.log('No se puede divir el numero')
+    }
+
+    return res;
+}
+
+// Funcion que retorna si un numero es Par o no
+function esPar(n1) {
+    // Va a dividir el numero hasta que sae 0
+    return n1 % 2 === 0;
+}
+
+console.log(Contraseña(123))
+console.log(Division(10,2))
+console.log(esPar(2))
 // Escribir un programa que ingrese una contraseña y retorne si la contraseña es correcta
 
