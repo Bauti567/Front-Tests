71ce63f1fea90cf659502e3dffd4abd700d2350c
Juan Bautista <jbautistapulido@gmail.com>
2024-04-24 18:00:31 -0500

    Learning DOM

---
+++ JavaScript/DOM/main.js
@@ -0,0 +1,7 @@
+// Para conectar el DOM con el script es necesario
+// Hacer uso de la etiqueta script y referenciar donde se encuentra el 
+// archivo del script
+
+alert('Mi primer practica con DOM')
+console.log('Aprendiendo el DOM')
+
