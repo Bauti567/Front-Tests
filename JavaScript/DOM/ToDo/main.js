612e1aeacf0ece08ab332963f68e50626115a4d4
Juan Bautista <jbautistapulido@gmail.com>
2024-04-25 18:37:17 -0500

    Todo App complete

--- JavaScript/DOM/ToDo/main.js
+++ JavaScript/DOM/ToDo/main.js
@@ -1,5 +1,8 @@
 // Vamos a crear un todo list para poder agregar elementos a una lista
 
+const todos = []; // Almacenar los todos
+
+
 window.onload = ()=>{
     // Obteniendo los valoes del DOM
     const todoForm = document.getElementById('todoForm')
@@ -9,7 +12,14 @@ window.onload = ()=>{
         const todo = document.getElementById('todo');
         const todoText = todo.value;
         todo.value = '';
-        console.log(todoText);
+        todos.push(todoText); // Agregar los elementos del arreglo
+        console.log(todoText); // MOSTRANDO EL VALOR DEL INPUT
+        console.log(todos)
+        const todoList = document.getElementById('list')
+        const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+        todoList.innerHTML = todosTemplate.join('');
+        
+        console.log(todosTemplate)
     }
 
 }
