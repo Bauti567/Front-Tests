b9dc8addc453da23d0e2ccefe60f93c07f0c3bfd
Juan Bautista <jbautistapulido@gmail.com>
2024-04-28 18:14:32 -0500

    Practice Local Storage

--- JavaScript/DOM/ToDo/main.js
+++ JavaScript/DOM/ToDo/main.js
@@ -3,6 +3,35 @@
 const todos = []; // Almacenar los todos
 const arr = [1,2,3,4,5]
 
+const render = ()=>{ // Creando la fincion render para crear porciones de codigo
+    const todoList = document.getElementById('list') // TR
+    const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+    todoList.innerHTML = todosTemplate.join('');
+    console.log(todosTemplate)
+    
+    // Va a traer todos los elementos dentro de ese ID de derecha a izauierda
+    const elementos = document.querySelectorAll('#todo-list li') // Trae todos los elementos dentro del HTML
+    // Iteramos con ForEach
+    elementos.forEach((elemento, i)=>{
+        // Añadir evento escuchador al elemento
+        elemento.addEventListener('click',()=>{
+            // Mientras se va iterando vamos agregando un evento
+            elemento.addEventListener('click', ()=>{
+                elemento.parentNode.removeChild(elemento)
+            })
+            // La funcion paren node referencia al padre 
+            console.log(elemento, i);
+            todos.splice(i,1);
+            console.log(todos, i);
+
+            
+        });
+    })
+
+}
+
+
+
 window.onload = ()=>{
     // Obteniendo los valoes del DOM
     const todoForm = document.getElementById('todoForm')
@@ -13,33 +42,11 @@ window.onload = ()=>{
         const todoText = todo.value;
         todo.value = '';
         todos.push(todoText); // Agregar los elementos del arreglo
-        console.log(todoText); // MOSTRANDO EL VALOR DEL INPUT
-        console.log(todos)
-        const todoList = document.getElementById('list') // TR
-        const todosTemplate = todos.map(t => '<li>' + t + '</li>');
-        todoList.innerHTML = todosTemplate.join('');
-        console.log(todosTemplate)
-        
-        // Va a traer todos los elementos dentro de ese ID de derecha a izauierda
-        const elementos = document.querySelectorAll('#todo-list li') // Trae todos los elementos dentro del HTML
-        // Iteramos con ForEach
-        elementos.forEach((elemento, i)=>{
-            // Añadir evento escuchador al elemento
-            elemento.addEventListener('click',()=>{
-                // Mientras se va iterando vamos agregando un evento
-                elemento.addEventListener('click', ()=>{
-                    elemento.parentNode.removeChild(elemento)
-                })
-                // La funcion paren node referencia al padre 
-                console.log(elemento, i);
-                todos.splice(i,1);
-                
-
-                
-            });
-        })
-        
+        render(); // Ejecuta el bloque superior   
     }
     
- 
 }
+
+// Local Storage permite almacenar elementos dentro del explorador Web
+// setItem, almacenar un elemento
+// getItem traer el elemento almacenado
