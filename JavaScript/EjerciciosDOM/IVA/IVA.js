22214df01c90156d11f06ca34f592b64ad44cd8c
Juan Bautista <jbautistapulido@gmail.com>
2024-04-29 17:37:19 -0500

    New app IVA modified

--- JavaScript/EjerciciosDOM/IVA/IVA.js
+++ JavaScript/EjerciciosDOM/IVA/IVA.js
@@ -1,14 +1,35 @@
 const container = document.getElementById('container')
+let res = document.getElementById('result')
+
+function Total() {
+    
+}
+
+function IVA(price) { 
+    if(isNaN(price)){
+        console.log('No se puede hacer la oprecion de IVA')
+        return NaN;
+    }
+    return price * 19 / 100
+}
 
 container.onsubmit = (e)=>{
     e.preventDefault();
-    let price = document.getElementById('price').value;
-    price.parseInt();
-    if (typeof price != "number" ) {
+    
+    let price = parseInt(document.getElementById('price').value);
+    if (isNaN(price)) {
+        // isNaN hace referencia a NOT A NUMBER
         console.log('No es un numero')
+        res.innerHTML = 'No es un número: '+ document.getElementById('price').value;
+
     } else {
-        console.log('Si es un numero')
+        console.log('Si es un numero');
+        let iva = IVA(price)
+        if (!isNaN(iva)) {
+            res.innerHTML = iva.toFixed(2);
+        }
     }
-    console.log(price);
+    
+    console.log(IVA(price))
 
 }
