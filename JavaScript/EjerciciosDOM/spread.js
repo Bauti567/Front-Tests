63fec95569717555a528918febeefe9a22b35daa
Juan Bautista <jbautistapulido@gmail.com>
2024-05-01 18:42:57 -0500

    New try with spread

--- JavaScript/EjerciciosDOM/spread.js
+++ JavaScript/EjerciciosDOM/spread.js
@@ -3,7 +3,20 @@
 
 // Se usa para crear una copia de una variable
 
-const a = { b:1 } // Permite tener constantes 
+const a = { b : 1 } // Permite tener constantes 
 // Que sean distintas entre si
 
+const b = a 
+
+console.log(b === a) // true
+// Ambos son similares
+// Ya que ambas son similares al momento de cambiar una sola se van a pasar esos cambios a la otra
+// dado que son una constante
+
+const c = { ...a }
+// En esta linea de codigo le estoy diciento que cree un objeto simiar a A
+console.log(a === c);
+// False ya que son objetos distintos
+
+/// ------------------- PROMISES ------------------------------
 
