5ca6f83632e8c8541f7dbd8920204b879e407764
Juan Bautista <jbautistapulido@gmail.com>
2024-05-02 17:16:46 -0500

    My first app with API express :)

---
+++ API-Rest/api.js
@@ -0,0 +1,12 @@
+// Creando mi primera API
+const express = require('express') // Asignacion de Framework
+
+const app = express()
+const port = 3000
+
+
+app.get('/', (req,res) =>{ // Request y Response
+    
+
+})
+
