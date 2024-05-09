74dcf2627b5dbdde7191f18bda72219ca8f8940a
Juan Bautista <jbautistapulido@gmail.com>
2024-05-09 17:23:51 -0500

    CRUD de mongo

--- API-Rest/index.js
+++ API-Rest/index.js
@@ -13,15 +13,61 @@ const User = mongoose.model('User',{
 
 // Funcion asyncrona para conectar
 const crear = async ()=>{
-    const user = new User({userName: 'Juan', edad: 23});
+    const user = new User({userName: 'Prueba', edad: 13});
     const saveUser = await user.save() // Almacenar usuario y retorna una promesa
     console.log(saveUser)
 
 }
 
 
-crear();
+// Con este tipo de funcion se pueden consultar todos los usuarios
+const buscarTodo = async ()=>{
+    const users = await User.find()
+    console.log(users)
+
+}
+
+// Buscar un usuario por username
+const buscar = async ()=>{
+    const users = await User.find({ userName: 'Busqueda' })
+    console.log(users);
+    // En este caso volví a registrar un usuario para poder enviar los datos
+
+}
+
+const buscarUno = async () =>{
+    const user = await User.findOne({userName: 'Prueba'})
+    // La funcion de FindOne retorna un objeto
+    console.log(user)
+}
+
+const actualizar = async ()=>{
+    const user = await User.findOne({userName: 'Chanchito triste', edad: 30})
+    console.log(user)
+    user.edad = 30
+    await user.save();
+    
+}
 
 
+//crear();
+//buscar();
+//buscarTodo();
+//buscarUno();
+// actualizar();
 
+const eliminar = async()=>{
+    const user = await User.find({userName: 'Chanchito triste'})
+    console.log(user)
+    // Eliminando al usuario de la BD
+    if (user) {
+        await user.remove()
+    
+    } else{
+        console.log('No existe papi')
+    }
+}
+
+
+eliminar();
 
