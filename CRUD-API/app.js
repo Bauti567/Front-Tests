1a793d767856647e94961375ffb9731deae15d26
Juan Bautista <jbautistapulido@gmail.com>
2024-05-16 17:44:47 -0500

    My first API app :)

--- CRUD-API/app.js
+++ CRUD-API/app.js
@@ -10,9 +10,23 @@ app.use(express.json())
 //mongoose.connect('mongodb+srv://PracticaJuan:3229282156@cluster0.tfqechz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 app.use(express.static(path.join(__dirname, 'routes')))
 
+
 // Rutas
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'routes','index.html'))
+})
+
+
+
+// No rutas
+app.use((err,req,res,next)=>{
+    res.status(404).send('Ruta no encontrada');
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err.stack)
+    res.stack(500).send('Error en el servidor')
+
 })
 
 
