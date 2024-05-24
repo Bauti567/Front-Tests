18710fe841a38a33a00a6818631e7738ea3bbac5
Juan Bautista <jbautistapulido@gmail.com>
2024-05-23 19:11:59 -0500

    Finally working my first app Auth

--- CRUD-AUTH/src/db.js
+++ CRUD-AUTH/src/db.js
@@ -1,1 +1,16 @@
 // Conexion con la base de datos
+
+const mongoose = require('mongoose');
+
+// Conexion con la base de datos
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect('mongodb+srv://JuanBautista:80274331Juan@atlascluster.lazttss.mongodb.net/authApp?retryWrites=true&w=majority&appName=AtlasCluster')
+        console.log('DB is conected')
+    } catch (error){
+        console.log('NO SE PUEDE CONECTAR', error);
+
+    }
+}
+
+module.exports = connectDB;
--- CRUD-AUTH/src/index.js
+++ CRUD-AUTH/src/index.js
@@ -1,7 +1,10 @@
 // Encargado de arrancar la app
 const app = require('./app.js')
+const connectDB = require('./db.js')
 const PORT = 3000
 
+
+connectDB();
 app.listen(PORT, ()=>{
     console.log('Server on Port', 3000);
 
