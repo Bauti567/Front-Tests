5fd0ca0c1a209b586514b3a1d88e4606d9baa4f5
Juan Bautista <jbautistapulido@gmail.com>
2024-06-03 17:08:39 -0500

    My first token

--- CRUD-AUTH/src/controllers/auth.controllers.js
+++ CRUD-AUTH/src/controllers/auth.controllers.js
@@ -1,23 +1,49 @@
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { token } from 'morgan';
 
 export const register = async (req, res) => {
-    const { email, password, username } = req.body;
+    const { email, password, username } = req.body;   
     
     try {
+        // Encriptado de contraseña
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User({
             userName: username,
             email,
-            password
+            password: passwordHash,
         });
         
+
         const userSaved = await newUser.save();
-        return res.json(userSaved);  // Aquí enviamos la respuesta y terminamos la ejecución
+        
+        jwt.sign({
+            id:userSaved._id
+        },'secret123',
+        {
+            expiresIn:"1d",
+        },(err,token)=>{
+            if (err) {
+                console.log(err)
+                res.json({token})
+            }
+            
+        });
+
+        return res.json({
+            id: userSaved.id,
+            username: userSaved.userName,
+            email: userSaved.email
+
+        });  // Aquí enviamos la respuesta y terminamos la ejecución
 
     } catch (error) {
         console.log('No se pudo registrar el usuario', error);
         return res.status(400).send('Error al registrar el usuario'); // Usamos return para terminar la ejecución aquí también
     }
+
 }
 
 export const login = (req, res) => {
