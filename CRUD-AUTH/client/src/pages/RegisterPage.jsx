4594694d60c36dcfad57ffe491d899509f60a987
Juan Bautista <jbautistapulido@gmail.com>
2024-06-10 14:14:52 -0500

    Im working at deharomas after 2 years

---
+++ CRUD-AUTH/client/src/pages/RegisterPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="username" />
+        <input type="email" name="email" />
+        <input type="passwprd" name="password" />
+        
+      </form>
+    </div>
+  )
+}
