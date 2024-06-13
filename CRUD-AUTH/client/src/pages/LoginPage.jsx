660d74056d5b37d8566f7c4789b7553dcbee0ef6
Juan Bautista <jbautistapulido@gmail.com>
2024-06-13 12:26:13 -0500

    Working at login page

--- CRUD-AUTH/client/src/pages/LoginPage.jsx
+++ CRUD-AUTH/client/src/pages/LoginPage.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage - No componente</div>
+    <div>
+      <form>
+
+        <h1>Este es mi formulario de login</h1>
+        <input type="email" className='container-input' placeholder='E-mail' />
+        
+        <input type="password" className='container-input'  placeholder='Password'/>
+
+        <button type="submit" className='container-button'>Enviar form</button>
+      </form>
+    </div>
   )
 }
