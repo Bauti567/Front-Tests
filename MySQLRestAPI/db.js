9b8e761c5d36ac682294df2dce5df57c8bed7cad
Juan Bautista <jbautistapulido@gmail.com>
2024-07-17 19:00:39 -0500

    Conecting the database

--- MySQLRestAPI/db.js
+++ MySQLRestAPI/db.js
@@ -1,12 +1,16 @@
 // Conexion con base de datos llamado Pool
-import { createPool } from "mysql2";
+import { createPool } from "mysql2/promise";
 
 
 // Parámetros de conexión
-createPool({
+export const pool = createPool({
     host: 'localhost', //-------- Aquí iría una IP ---------
     user: 'root',
     password: 'JuanDev*$1',
     port: 3306,
     database: 'companydb'    
 })
+
+
+// El query permite generar consultas SQL
+pool.query()
--- MySQLRestAPI/index.js
+++ MySQLRestAPI/index.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import {pool} from './db.js'
 
 const app = express()
 const PORT = 3000
 
 
 // Routes
+
+app.get('/ping', async(req,res)=>{
+    const [result] = await pool.query('SELECT ')
+    res.json(result[0])
+})
+
 app.get('/employees',(req,res)=>{res.send('Obteniendo empleados')})
 app.post('/employees',(req,res)=>{res.send('Creando empleados')})
 app.put('/employees',(req,res)=>{res.send('Actualizando empleados')})
