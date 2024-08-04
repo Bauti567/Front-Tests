a4875c5a82bacb8b5489c6c861a21d0331703fd5
Juan Bautista <jbautistapulido@gmail.com>
2024-08-04 17:01:23 -0500

    This poject ill be deleted

--- CRUD-API/app.js
+++ CRUD-API/app.js
@@ -10,3 +10,4 @@ app.listen(PORT,()=>{
     console.log('APP IS RUNNING ', PORT)
 
 })
+
--- CRUD-API/controllers/authControllers.js
+++ CRUD-API/controllers/authControllers.js
@@ -1,4 +1,5 @@
 export const base = (req,res)=>{
     res.send("La base de mi api")
     console.log('Esta es la base')
+
 }
