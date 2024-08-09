e96839fc5656177a8419ed15e8fd07b3ae80e50c
Juan Bautista <jbautistapulido@gmail.com>
2024-08-08 19:21:52 -0500

    Working at login route

--- Deharomas/src/controllers/auth.controller.js
+++ Deharomas/src/controllers/auth.controller.js
@@ -13,8 +13,7 @@ export const Register = async(req,res)=>{
 
 }
 
-
-export const Login = (req,res)=>{
-   res.send('Login page') 
-
+export const Login = async(req,res)=>{
+   const {email,password} = req.body
+   const [rows] = await pool.query('Select FROM')
 }
