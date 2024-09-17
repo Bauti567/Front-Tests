598929182256159765a06d2ba623545ac0c5477c
Juan Bautista <jbautistapulido@gmail.com>
2024-09-16 20:34:11 -0500

    New repo initialized News aPI

--- News-Front/src/Components/NewsList.jsx
+++ News-Front/src/Components/NewsList.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react'
 
 function NewsList() {
-  const [news,SetNews] = useState([]);
-  useEffect(()=>{
+    const [news,setNews] = useState([]);
 
-  },[])
-  
-  function fetchData(params) {
-    const data = fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-08-16&sortBy=publishedAt&apiKey=202c54be158b431f959b07c54170da51')
-    console.log(data);
+    // cada vez que se renderice al componete   
+    useEffect(()=>{
+        async function fetchData(params) {
+            const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-08-16&sortBy=publishedAt&apiKey=202c54be158b431f959b07c54170da51')
+            const data = await response.json()
+            setNews(data.articles)
+            console.log(data.articles)
+        
+        }
+        fetchData();
+        
+    },[])
 
-  }
-
-  fetchData();
-
-  return (
-    <div>NewsList</div>
+    return (
+    <div>
+        {
+            news.map(news=>{
+                
+            })
+        }
+    </div>
   )
 }
 
--- News-Front/src/main.jsx
+++ News-Front/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import News from './Components/News.jsx'
+import NewsList from './Components/NewsList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <News/>
+    <NewsList/>
   </StrictMode>,
 )
