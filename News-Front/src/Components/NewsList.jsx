f55443af0fdb5a9789c8cff969882d998da5041c
Juan Bautista <jbautistapulido@gmail.com>
2024-09-18 14:16:13 -0500

    The API is loading

--- News-Front/src/Components/NewsList.jsx
+++ News-Front/src/Components/NewsList.jsx
@@ -3,19 +3,32 @@ import News from "./News";
 
 function NewsList() {
   const [news, setNews] = useState([]);
+  const [loading,setLoading] = useState(true)
+
 
   // cada vez que se renderice al componete
   useEffect(() => {
     async function fetchData(params) {
-      const response = await fetch(
-        "https://newsapi.org/v2/everything?q=bitcoin&apiKey=202c54be158b431f959b07c54170da51"
-      );
-      const data = await response.json();
-      setNews(data.articles);
-      console.log(data.articles);
+      try{
+        const response = await fetch(
+          "https://newsapi.org/v2/everything?q=bitcoin&apiKey=202c54be158b431f959b07c54170da51"
+        );
+        const data = await response.json();
+        setNews(data.articles);
+        console.log(data.articles);
+      } catch(error){
+        console.log('Error fetching News', error)
+        
+      } finally{
+        setLoading(false)
+      }
     }
     fetchData();
   }, []);
+
+  if(loading){
+    return <div className="loading">Loading ....</div>
+  }
 
   return (
     <div className="container mt-5">
