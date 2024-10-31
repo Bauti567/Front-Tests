2ef9420f618a0d1afcd5bfab78eb465abb48b4d9
Juan Bautista <jbautistapulido@gmail.com>
2024-10-31 18:01:00 -0500

    The repository

--- CATS-TEST/bakend/src/controllers/auth.controller.js
+++ CATS-TEST/bakend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 import { createAccessToken } from '../libs/jwt.js'
+import jwt from 'jsonwebtoken'
 
 
 export const register = async(req,res)=>{
