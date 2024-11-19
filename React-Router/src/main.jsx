93de69020fa46fc0b4bd0d13432627fc56d0c14b
Juan Bautista <jbautistapulido@gmail.com>
2024-11-19 17:53:18 -0500

    Working react router Outlet

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { 
-  createBrowserRouter, 
-  RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'    
 import Root from './routes/root'
 import ErrorPage from './error-page'
+import Contact from './routes/contact'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/", // ruta de inicio
     element: <Root/>,
-    errorElement: <ErrorPage/>
-    
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>
+      }
+    ]
   }
-])  
-    
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- React-Router/src/routes/root.jsx
+++ React-Router/src/routes/root.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 export default function Root() {
     return (
       <>
@@ -38,6 +40,9 @@ export default function Root() {
           </nav>
         </div>
         <div id="detail"></div>
+        <div id="detail">
+            <Outlet/>    
+        </div> 
       </>
     );
   }
