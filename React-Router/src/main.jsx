26f043dff8019f4094915682d17124719ee5e51b
Juan Bautista <jbautistapulido@gmail.com>
2024-11-27 17:23:20 -0500

    Re installed react router repository

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -1,29 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'    
-import Root from './routes/root'
-import ErrorPage from './error-page'
-import Contact from './routes/contact'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/", // ruta de inicio
-    element: <Root/>,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact/>
-      
-      }
-      
-    ]
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
