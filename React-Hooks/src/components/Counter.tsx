6ca4f5949afe22b146b7897b1aab62cd229d7f15
Juan Bautista <jbautistapulido@gmail.com>
2025-01-08 19:10:19 -0500

    The firts useState exercise is working

--- React-Hooks/src/components/Counter.tsx
+++ React-Hooks/src/components/Counter.tsx
@@ -2,18 +2,27 @@
 import { useState } from "react";
 
 function Counter() {
-    const [plus, setPlus] = useState(0)
-    const [minus, setMinus] = useState(0)
-    
+    const [total,setTotal] = useState(0)
+
+    const sumar = ()=>{
+        setTotal(total + 1);
+    }
+    const restar = ()=>{
+        setTotal(total - 1)
+    }
+    const reset = ()=>{
+        setTotal(0)
+    }
     return (
         <div>Counter
             <div>
-                <button onClick={}>
-                    Sumar
-                </button>
-                <button onClick={}>
-                    Restar
-                </button>
+                <span>
+                    El total es {total}
+                </span>
+                <button onClick={sumar}> Sumar </button>
+                <button onClick={restar}> Restar </button>
+                <button onClick={reset}> Reset </button>
+                 
             </div>
         </div>
     )
--- React-Hooks/src/main.tsx
+++ React-Hooks/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Counter from './components/Counter.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     {/* Ejercicio 1 */}
+    <Counter/>
+     {/* Ejercicio 2 */}
+    
   </StrictMode>,
 )
