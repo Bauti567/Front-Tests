e6ac5f4b0268397eac8a16b40ec6d1a2cbb5ed9d
Juan Bautista <jbautistapulido@gmail.com>
2025-02-12 19:41:15 -0500

    Working on a new repo

---
+++ React-filters/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div>
+      <h1>El app</h1>
+      <Search/>
+      
+    </div>
+  )
+}
+
+
+
+export default App;
---
+++ React-filters/src/components/Search.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+function Search() {
+  // setear los use state
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // funcion para traer datos
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      console.log(data);
+      setUsers(data);
+
+    }
+
+    fetchData();
+  }, []);
+  // metodos para el filtrado de datos
+  const results = !search ? users : users.filter((dato)=> dato.name.toLoweCase().includes)
+
+  // fucion de busqueda
+  const Searcher = (event) =>{
+    setSearch(event.target.value);
+    console.log(event.target.value)
+  }
+  
+  // renderizado de la vista del componente
+
+  return (
+    <div>
+      <input onChange={Searcher} value={Searcher} type="text" placeholder="search" className="form-control"/>
+      <h2>Tabla con filtros</h2>
+      <table className="table table-striped table-hover mt-5 shadow-lg">
+        <thead>
+          <tr className="bg-curso text-white">
+            <th>Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((user)=>{
+              return(
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+              </tr>
+            )})
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Search;
