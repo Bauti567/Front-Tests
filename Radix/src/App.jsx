73334cf0a07ce1e207f1ef027b080314150a8783
Juan Bautista <jbautistapulido@gmail.com>
2025-03-13 18:58:48 -0500

    Working new UI library

---
+++ Radix/src/App.jsx
@@ -0,0 +1,16 @@
+import { Switch, Theme, Button, Slider, Progress, Card } from "@radix-ui/themes";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Switch></Switch>
+
+      <Button>Hola Mundo</Button>
+      <Progress></Progress>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
